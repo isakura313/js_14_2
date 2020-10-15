9fc6ad344fb792a803fbd7bf7284003dd40c1cf6
Paul <isakura313@gmail.com>
2020-10-15 20:48:00 +0300

    asdasd

--- website/code.js
+++ website/code.js
@@ -2,18 +2,27 @@ const input = document.querySelector("input");
 const btn = document.querySelector("button");
 const out_text = document.querySelector("#out_text");
 let selected = document.querySelector("select")
+let img = document.querySelector("img");
+
+// https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Heart_coraz%C3%B3n.svg/1200px-Heart_coraz%C3%B3n.svg.png
 
 
 const male = {
     greet: ["Дорогой", "Уважаемый", "Любимый"],
-    quality: ["Замечательным", "Прекрасным"],
-    state: ["таким же работящим", "таким же невероятным"]
+    quality: ["Замечательным", "Прекрасным", "Классным"],
+    state: ["таким же работящим", "таким же невероятным"],
+    url_images: ["https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Heart_coraz%C3%B3n.svg/1200px-Heart_coraz%C3%B3n.svg.png",
+        "https://www.smdp.com/wp-content/uploads/2020/04/BUNNY_-Snowflake.jpg",
+        "https://miro.medium.com/max/12032/1*99r7mbdm30qA1xrSJBuQjw.jpeg"]
 
 }
 const female = {
     greet: ["Дорогая", "Уважаемая", "Любимая"],
-    quality: ["Замечательным", "Прекрасным"],
-    state: ["такой же прекрасной", "такой же невероятной"]
+    quality: ["Замечательной", "Прекрасной"],
+    state: ["такой же прекрасной", "такой же невероятной"],
+    url_images: ["https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Heart_coraz%C3%B3n.svg/1200px-Heart_coraz%C3%B3n.svg.png",
+    "https://www.smdp.com/wp-content/uploads/2020/04/BUNNY_-Snowflake.jpg",
+    "https://miro.medium.com/max/12032/1*99r7mbdm30qA1xrSJBuQjw.jpeg"]
 }
 
 function sayText(sex) {
@@ -23,6 +32,7 @@ function sayText(sex) {
     Желаем вас оставаться всегда ${sex.state[getRandomInt(sex.state.length)]}.
     До новых встреч!
     `;
+    input.value = "";
     return text;
 }
 
@@ -33,9 +43,11 @@ btn.onclick = () => {
     }else if(selected.value == "female")
     {
         out_text.textContent = sayText(female);
+        img.src = female.url_images[getRandomInt(female.url_images.length)];
     }else if(selected.value == "male")
     {
         out_text.textContent = sayText(male);
+        img.src = male.url_images[getRandomInt(male.url_images.length)];
     }
 }
 

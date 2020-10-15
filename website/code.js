2213ff22bb62211acb9a190edc7d805b10d22783
Paul <isakura313@gmail.com>
2020-10-15 20:38:21 +0300

    asdasd

--- website/code.js
+++ website/code.js
@@ -1,24 +1,42 @@
 const input = document.querySelector("input");
 const btn = document.querySelector("button");
 const out_text = document.querySelector("#out_text");
+let selected = document.querySelector("select")
 
 
-const man = {
-    greet: ["Дорогой, Уважаемый"],
+const male = {
+    greet: ["Дорогой", "Уважаемый", "Любимый"],
+    quality: ["Замечательным", "Прекрасным"],
+    state: ["таким же работящим", "таким же невероятным"]
+
 }
 const female = {
-    greet: ["Дорогая, Уважаемая"],
-
+    greet: ["Дорогая", "Уважаемая", "Любимая"],
+    quality: ["Замечательным", "Прекрасным"],
+    state: ["такой же прекрасной", "такой же невероятной"]
 }
 
 function sayText(sex) {
     let text = `${sex.greet[getRandomInt(sex.greet.length)]}
-    ${input.value}`;
+    ${input.value}.  Хотим поздравить вас с этим 
+    ${sex.quality[getRandomInt(sex.quality.length)]} праздником.
+    Желаем вас оставаться всегда ${sex.state[getRandomInt(sex.state.length)]}.
+    До новых встреч!
+    `;
     return text;
 }
 
 btn.onclick = () => {
-    out_text.textContent = sayText(female);
+    if(selected.value == "None"){
+        alert("Пожалуйста, укажите пол коллеги!");
+        return
+    }else if(selected.value == "female")
+    {
+        out_text.textContent = sayText(female);
+    }else if(selected.value == "male")
+    {
+        out_text.textContent = sayText(male);
+    }
 }
 
 function getRandomInt(max) {
